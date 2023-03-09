f8525bb630263a2381a9d7546b862a0a7ada7a6d
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2023-03-09 15:03:09 -0300

    armazenar dados

---
+++ armazenar-dados.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# Variáveis para armazenar senhas dos usuários
+SENHA=$(openssl passwd -crypt Senha123)
+
+# Verifica se os diretórios já existem e, caso contrário, os cria
+if [ ! -d "/publico" ]; then
+    mkdir /publico
+    echo "Diretório /publico criado"
+fi
+
+if [ ! -d "/adm" ]; then
+    mkdir /adm
+    echo "Diretório /adm criado"
+fi
+
+if [ ! -d "/ven" ]; then
+    mkdir /ven
+    echo "Diretório /ven criado"
+fi
+
+if [ ! -d "/sec" ]; then
+    mkdir /sec
+    echo "Diretório /sec criado"
+fi
+
+# Verifica se os grupos já existem e, caso contrário, os cria
+if [ ! $(getent group GRP_ADM) ]; then
+    groupadd GRP_ADM
+    echo "Grupo GRP_ADM criado"
+fi
+
+if [ ! $(getent group GRP_VEN) ]; then
+    groupadd GRP_VEN
+    echo "Grupo GRP_VEN criado"
+fi
+
+if [ ! $(getent group GRP_SEC) ]; then
+    groupadd GRP_SEC
+    echo "Grupo GRP_SEC criado"
+fi
+
+# Cria os usuários especificando as senhas e grupos correspondentes
+useradd carlos -m -s /bin/bash -p $SENHA -G GRP_ADM
+useradd maria -m -s /bin/bash -p $SENHA -G GRP_ADM
+useradd joao -m -s /bin/bash -p $SENHA -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $SENHA -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $SENHA -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $SENHA -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $SENHA -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $SENHA -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $SENHA -G GRP_SEC
+
+# Especifica as permissões dos diretórios criados
+chown root:GRP_ADM /adm
+chmod 770 /adm
+
+chown root:GRP_VEN /ven
+chmod 770 /ven
+
+chown root:GRP_SEC /sec
+chmod 770 /sec
+
+echo "Usuários, grupos e diretórios criados com sucesso!"
---
+++ criar-user.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Define uma lista de usuários e senhas
+USERS=(
+  guest10:Senha123
+  guest11:Senha123
+  guest12:Senha123
+  guest13:Senha123
+)
+
+# Define uma função para criar e configurar cada usuário
+function create_user {
+  local username=$1
+  local password=$2
+  
+  # Verifica se o usuário já existe
+  if id "$username" >/dev/null 2>&1; then
+    echo "Usuário $username já existe. Pulando..."
+    return
+  fi
+  
+  # Cria o usuário com a senha definida
+  useradd "$username" -c "Usuário convidado" -s /bin/bash -m -p "$(openssl passwd -crypt "$password")"
+  
+  # Define a senha como expirada, forçando o usuário a alterá-la no primeiro login
+  chage -d 0 "$username"
+  
+  echo "Usuário $username criado com sucesso."
+}
+
+# Cria cada usuário da lista
+for user in "${USERS[@]}"; do
+  create_user "${user%:*}" "${user#*:}"
+done
+
+echo "Usuários criados com sucesso!"
